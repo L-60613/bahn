f8c8b82e188dfa0f46cde6c1533e373c5f0a5f03
L-60613 <lisa.jena.23@gmail.com>
2022-07-20 11:25:55 +0200

    Start Project

---
+++ src/main/java/com/example/bahn/BahnApplication.java
@@ -0,0 +1,13 @@
+package com.example.bahn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahnApplication.class, args);
+	}
+
+}
